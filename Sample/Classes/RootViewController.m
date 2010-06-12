c3d436c81e5e7b06c1cbe59384ec75703670db73
digdog <digdog@mac.com>
2010-06-12 16:29:48 -0700

    Use dynamic badge text

--- Sample/Classes/RootViewController.m
+++ Sample/Classes/RootViewController.m
@@ -82,35 +82,29 @@
 	// Configure the cell.
 	cell.summary = @"Summary";
 	cell.detail = @"Detail text goes here";
-	cell.badgeText = @"Badge Text";
+	cell.badgeText = [NSString stringWithFormat:@"Row %d", indexPath.row];
 	cell.badgeColor = [UIColor orangeColor];
 
     return cell;
 }
 
 	
-/*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
 
-
-/*
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source.
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
     }   
     else if (editingStyle == UITableViewCellEditingStyleInsert) {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
     }   
 }
-*/
 
 
 /*
